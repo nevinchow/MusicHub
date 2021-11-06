bd6f72e1c56835c6ed3901fb6b5c7b6db18fdd2c
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-11-06 09:26:38 -0500

    added artist model

---
+++ app/models/artist.py
@@ -0,0 +1,21 @@
+from .db import db
+
+class Artist():
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    bio = db.Column(db.String, nullable=False)
+    profileURL = db.Column(db.String)
+    headerURL = db.Column(db.String)
+    biopictureURL = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'bio': self.bio,
+            'profileURL': self.profileURL,
+            'headerURL': self.headerURL,
+            'biopictureURL': self.biopictureURL
+        }
