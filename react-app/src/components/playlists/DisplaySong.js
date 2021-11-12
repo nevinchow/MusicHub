70e06e1a2fb6e6a8eb2436ba5e0e08070113fb8d
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-11-12 09:01:26 -0600

    added styling to add button and fixed bug

--- react-app/src/components/playlists/DisplaySong.js
+++ react-app/src/components/playlists/DisplaySong.js
@@ -5,8 +5,8 @@ import { addSongToPlaylist } from "../../store/playlists_songs";
 import { useSelector } from "react-redux";
 import Song from '../songs/Song'
 import AddToPlaylist from "./addSongtoPlaylist";
-
-
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function DisplaySong({songId}) {
   const albums = useSelector(state => state.album)
@@ -118,7 +118,7 @@ function DisplaySong({songId}) {
               </Link>
             </td>
             <td className="cell">{trackTime}</td>
-            <td><button id={song.id} onClick={openSettings}>+</button> 
+            <td><FontAwesomeIcon className="plus-button" id={song.id} onClick={openSettings} icon={faPlus} /> 
             {settings ? <AddToPlaylist songId={songId}/> : <></>}
             </td>
         
@@ -130,80 +130,3 @@ function DisplaySong({songId}) {
 
 export default DisplaySong
 
-{/* 
-                //turn duration to string
-                trackNumber++;
-                const minutes = Math.floor(song.duration / 60);
-                const seconds = song.duration % 60;
-                let newSeconds;
-                if (seconds < 10) {
-                  newSeconds = `0${seconds}`;
-                } else {
-                  newSeconds = seconds;
-                }
-                let trackTime = ``;
-                if (minutes === 0) {
-                  trackTime = `${minutes}: ${newSeconds}`;
-                } else {
-                  trackTime = `${minutes} : ${newSeconds}`;
-                }
-                //get album for song
-                const thisAlbum = Object.keys(albums).find(
-                  (oneAlbum) => song.albumId === +oneAlbum
-                );
-
-                //get artist for song
-                const thisArtist = Object.keys(artists).find(
-                  (oneArtist) => song.artistId === +oneArtist
-                );
-
-                return (
-                  <tr
-                    className="table-row"
-                    // key={song.id}
-                    onMouseEnter={showPlayButton}
-                    onMouseLeave={hidePlayButton}
-                  >
-                    {play ? (
-                      <td className="cell">
-                        <Song songId={song.id} />
-                      </td>
-                    ) : (
-                      <></>
-                    )}
-                    <td className="cell track">{trackNumber}</td>
-                    <td className="cell">
-                      <img
-                        className="album-thumbnail"
-                        src={albums[thisAlbum].imageURL}
-                        alt={albums[thisAlbum].name}
-                      ></img>
-                    </td>
-                    <td className="cell">
-                      <div className="song-name-row">
-                        <p>{song.name}</p>
-                        <Link
-                          className="song-info-links"
-                          to={`/artist/${artists[thisArtist].id}`}
-                        >
-                          {artists[thisArtist].name}
-                        </Link>
-                      </div>
-                    </td>
-                    <td className="cell">
-                      <Link
-                        className="song-info-links"
-                        to={`/albums/${albums[thisAlbum].id}`}
-                      >
-                        {albums[thisAlbum].title}
-                      </Link>
-                    </td>
-                    <td className="cell">{trackTime}</td>
-                    <td><button id={song.id} onClick={openSettings}>+</button> 
-                    {settings ? <AddToPlaylist songId={songId}/> : <></>}
-                    </td>
-                
-                  </tr>
-                );
-              })}
-            </tbody> */}
