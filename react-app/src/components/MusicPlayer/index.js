1689059dd0c07fd0e8e820488e77d7677f595858
Ayla Basha <aylabasha@gmail.com>
2021-11-11 17:18:28 -0800

    fixed player issue

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -1,28 +1,37 @@
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
 // import styled from "styled-components";
-import './musicPlayer.css';
+import "./musicPlayer.css";
+
+  
 
 // import 'react-h5-audio-player/lib/styles.less' Use LESS
 // import 'react-h5-audio-player/src/styles.scss' Use SASS
 
+const Player = () => {
+  const queue = useSelector((state) => state.musicQueue);
+  return (
+    <>
+      {console.log(queue, "THIS IS A TEST")}
+
+      <AudioPlayer
+        className="audio-player"
+        autoPlayAfterSrcChange={true}
+        src="https://res.cloudinary.com/dexkxkrfp/video/upload/v1636396215/Albums/Surrender/05_Alive_mbqvdv.m4a"
+        onPlay={(e) => console.log("onPlay")}
+        // other props here
+      />
+    </>
+  );
+} 
 
 
-const Player = ({queue}) => (
-  <>
- {console.log(queue, 'THIS IS A TEST')}
-  
-  <AudioPlayer className="audio-player"
-   Pause src='https://res.cloudinary.com/dso59ikqw/video/upload/v1636391731/yt5s.com-02._The_Weeknd_-_The_Town_HD_uf0k9v.mp4'
-    onPlay={(e) => console.log("onPlay")}
-    // other props here
-  />
-  </>
-);
+  //  autoPlayAfterSrcChange={true}
+  //       src={queue[0].song_link}
 
 // const playerCSS = styled.div`
-// `; 
+// `;
 
 export default Player;
