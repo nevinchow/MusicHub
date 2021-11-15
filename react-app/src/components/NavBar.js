9160e0ca5f9ba0b0fb6a8a7918352d28a72bd876
Nevin Chow <nevinchow@gmail.com>
2021-11-14 23:44:12 -0800

    done

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import { useSelector } from "react-redux";
+import logo from '/home/nevinchow/module6/python-project-starter/react-app/src/images/spotifyLogo2.png'
+
 
 const NavBar = () => {
   const sessionUser = useSelector((state) => state.session.user);
   if (sessionUser) {
     return (
       // Upper Nav Bar
-      <nav style={{backgroundColor: 'black', height: '100px'}}>
+      <nav style={{backgroundColor: 'black', height: '100px', display: 'flex'}}>
         <ul>
           <li>
             <NavLink to='/' exact={true} activeClassName='active'>
@@ -31,13 +33,19 @@ const NavBar = () => {
   } else {
     return (
       <>
-      <nav style={{backgroundColor: 'black', height: '100px'}}>
-      <li style={{position: 'relative', left: '70%', top: '45px'}}>
+      <NavLink to='/'>
+
+        <img className='spotifyLogo'src={logo} style={{position: 'absolute', backgroundColor: 'black', height: '99px', width:'99px', marginLeft: '10px'}}></img>
+      </NavLink>
+
+      <nav style={{backgroundColor: 'black', height: '100px', display:'flex', justifyContent:'flex-end', marginRight:'300px', alignItems: 'center', width: '99vw'}}>
+        <NavLink to='/' style={{marginRight: '84%'}}><h2 style={{color:'green'}}>Music Hub</h2></NavLink>
+      <li >
             <NavLink to='/login' exact={true} activeClassName='active' style={{color: 'white', fontWeight: 'bold'}}>
               Login
             </NavLink>
           </li>
-          <li style={{position: 'relative', left: '65%', top: '30px'}}>
+          <li>
             <NavLink to='/sign-up' exact={true} activeClassName='active' style={{textDecoration: 'none', color: 'white', fontWeight: 'bold'}}>
               Sign Up
             </NavLink>
