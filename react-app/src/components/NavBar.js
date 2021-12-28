ed5f01dde2ea86777c51c875dec78acf8adcbba0
Ayla Basha <aylabasha@gmail.com>
2021-12-27 19:36:44 -0800

    added media query screens

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,26 +1,27 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 import { useSelector } from "react-redux";
 // import logo from '/Users/yingjiazhuo/Desktop/python/SpotifyProject/react-app/src/images/spotifyLogo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 const NavBar = () => {
   const sessionUser = useSelector((state) => state.session.user);
   if (sessionUser) {
     return (
       // Upper Nav Bar
-      <nav style={{backgroundColor: 'black', height: '100px', display: 'flex'}}>
+      <nav
+        style={{ backgroundColor: "black", height: "100px", display: "flex" }}
+      >
         <ul>
           <li>
-            <NavLink to='/' exact={true} activeClassName='active'>
+            <NavLink to="/" exact={true} activeClassName="active">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to='/playlists' exact={true} activeClassName='active'>
+            <NavLink to="/playlists" exact={true} activeClassName="active">
               Playlists
             </NavLink>
           </li>
@@ -30,32 +31,60 @@ const NavBar = () => {
         </ul>
       </nav>
     );
-
   } else {
     return (
       <>
-<<<<<<< HEAD
-      <nav style={{backgroundColor: 'black', height: '100px'}}>
-      <li style={{position: 'relative', left: '70%', top: '50px'}}>
-            <NavLink to='/login' exact={true} activeClassName='active' style={{color: 'white', fontWeight: 'bold'}}>
-=======
-      <NavLink to='/'>
+        <NavLink to="/">
+          <FontAwesomeIcon
+            icon={faSpotify}
+            className="spotifyLogo"
+            style={{
+              position: "absolute",
+              color: "white",
+              height: "75px",
+              width: "75px",
+              marginLeft: "4%",
+              margin: "10px",
+            }}
+          />
+        </NavLink>
 
-        <FontAwesomeIcon icon={faSpotify} className='spotifyLogo' style={{position: 'absolute', color: 'white', height: '99px', width:'99px', marginLeft: '10px'}}/>
-      </NavLink>
-
-      <nav style={{backgroundColor: 'black', height: '100px', display:'flex', justifyContent:'flex-end', marginRight:'300px', alignItems: 'center', width: '99vw'}}>
-            <NavLink to='/login' exact={true} activeClassName='active' style={{color: 'white', fontWeight: 'bold', marginRight: '3%'}}>
->>>>>>> 6e5477c82c10bbdb031e6b7171df93d74c8c0e4e
-              Login
-            </NavLink>
-            <NavLink to='/sign-up' exact={true} activeClassName='active' style={{textDecoration: 'none', color: 'white', fontWeight: 'bold', marginRight: '3%'}}>
-              Sign Up
-            </NavLink>
-          </nav>
-          </>
-    )
+        <nav
+          style={{
+            backgroundColor: "black",
+            height: "100px",
+            display: "flex",
+            justifyContent: "flex-end",
+            marginRight: "300px",
+            alignItems: "center",
+            width: "99vw",
+          }}
+        >
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            style={{ color: "white", fontWeight: "bold", marginRight: "3%" }}
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontWeight: "bold",
+              marginRight: "3%",
+            }}
+          >
+            Sign Up
+          </NavLink>
+        </nav>
+      </>
+    );
   }
-}
+};
 
 export default NavBar;
