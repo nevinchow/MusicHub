a7cfab6b904723bde6aad53cd7a322d7de1233b8
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-12-29 15:33:01 -0600

    fixed white space in footer on landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -7,6 +7,9 @@ import logo from '../../images/github-logo.png'
 import logo2 from '../../images/linkedin-logo.png'
 import text from '../../images/text.png'
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function LandingPage() {
     const dispatch = useDispatch()
@@ -24,51 +27,80 @@ function LandingPage() {
 
 
     return (
-        <>
+      <>
         <div className="landingPageContainer">
-        <div className="landingBackground">
-            <button className="demoButton" onClick={handleSubmit}>LOG IN AS A DEMO USER</button>
+          <div className="landingBackground">
+            <div className="vibing-container">
+              <div className="header1">
+                <h1 className="vibing">Vibing</h1>
+              </div>
+              <div className="header2">
+                <h1 className="everything">is everything</h1>
+              </div>
+            </div>
+            <h2 className="tagline">
+              Millions of songs, no credit card needed
+            </h2>
+            <button className="demoButton" onClick={handleSubmit}>
+              LOG IN AS A DEMO USER
+            </button>
+            <div className="footer">
+              <div className="footerLinksContainer">
+                <h2 className="footerTitle">Nevin Chow</h2>
+                <div className="media-links">
+                  <a href="https://github.com/nevinchow">
+                    <FontAwesomeIcon className="media-icon" icon={faGithub} />
+                  </a>
+                  <a href="https://www.linkedin.com/in/nevin-chow-aa4770221/">
+                    <FontAwesomeIcon
+                      className="media-icon"
+                      icon={faLinkedinIn}
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="footerLinksContainer">
+                <h2 className="footerTitle"> Jessi Zhuo</h2>
+                <div className="media-links">
+                  <a href="https://github.com/zyingzhuo">
+                    <FontAwesomeIcon className="media-icon" icon={faGithub} />
+                  </a>
+                  <a href="https://www.linkedin.com/in/yingjia-zhuo-25a474170/">
+                    <FontAwesomeIcon
+                      className="media-icon"
+                      icon={faLinkedinIn}
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="footerLinksContainer">
+                <h2 className="footerTitle">Ayla Basha</h2>
+                <div className="media-links">
+                  <a href="https://github.com/spacegray">
+                    <FontAwesomeIcon className="media-icon" icon={faGithub} />
+                  </a>
+                  <FontAwesomeIcon className="media-icon" icon={faLinkedinIn} />
+                </div>
+              </div>
+              <div className="footerLinksContainer">
+                <h2 className="footerTitle">Dusty Mumphrey</h2>
+                <div className="media-links">
+                  <a href="https://github.com/Dusttoo">
+                    <FontAwesomeIcon className="media-icon" icon={faGithub} />
+                  </a>
+                  <a href="https://www.linkedin.com/in/dusty-mumphrey/">
+                    <FontAwesomeIcon
+                      className="media-icon"
+                      icon={faLinkedinIn}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <footer className="footerContainer">
-            <div className="footerLinksContainer">
-        <h2 className="footerTitle">Nevin Chow</h2>
-              <a href="https://github.com/nevinchow">
-              <img className="githubLogo" src={logo} height={40} width={40} alt=""/>
-              </a>
-              <a href='https://www.linkedin.com/in/nevin-chow-aa4770221/'>
-              <img className="linkedinLogo" src={logo2} height={40} width={40} alt=""/>
-              </a>
-            </div>
-            <div className="footerLinksContainer">
-              <h2 className='footerTitle'> Jessi Zhuo</h2>
-              <a href='https://github.com/zyingzhuo'>
-              <img className="githubLogo" src={logo} height={40} width={40} alt=""/>
-              </a>
-              <a href='https://www.linkedin.com/in/yingjia-zhuo-25a474170/'>
-              <img className="linkedinLogo" src={logo2} height={40} width={40} alt=""/>
-              </a>
-            </div>
-            <div className="footerLinksContainer">
-              <h2 className='footerTitle'>Ayla Basha</h2>
-              <a href='https://github.com/spacegray'>
-              <img className="githubLogo" src={logo} height={40} width={40} alt=""/>
-              </a>
-              <img className="linkedinLogo" src={logo2} height={40} width={40} alt=""/>
-            </div>
-            <div className="footerLinksContainer">
-              <h2 className='footerTitle'>Dusty Mumphrey</h2>
-              <a href='https://github.com/Dusttoo'>
-              <img className="githubLogo" src={logo} height={40} width={40} alt=""/>
-              </a>
-              <a href='https://www.linkedin.com/in/dusty-mumphrey/'>
-              <img className="linkedinLogo" src={logo2} height={40} width={40} alt=""/>
-              </a>
-            </div>
-        </footer>
-
-        </div>
-        </>
-    )
+      </>
+    );
 }
 
 
