c89fc71055afaf303ca278cafd3aadbf9463cca5
Ayla Basha <aylabasha@gmail.com>
2022-01-24 11:59:38 -0800

    updated context filename

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-import  CurrentSongsProvider  from './context/queue';
+import CurrentSongsProvider from './context/queue.js';
 
 const store = configureStore();
 
